5ace4d77ae9579297d00f26b068eca401f435723
shiva <tejabttr@gmail.com>
2018-12-15 22:26:29 +0530

    removed sysouts

--- src/main/java/com/techstuff/restapi/SpringRestApiApplication.java
+++ src/main/java/com/techstuff/restapi/SpringRestApiApplication.java
@@ -8,9 +8,5 @@ public class SpringRestApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringRestApiApplication.class, args);
-		System.out.println("first");
-		System.out.println("second");
-		System.out.println("fourth");
-		System.out.println("third");
 	}
 }
