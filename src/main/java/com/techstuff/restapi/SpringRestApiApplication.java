30de5b374c953e153de913d424c16fa51245b57b
tejabttr <tejabttr@gmail.com>
2018-12-15 19:50:16 +0530

    Update SpringRestApiApplication.java
    updated

--- src/main/java/com/techstuff/restapi/SpringRestApiApplication.java
+++ src/main/java/com/techstuff/restapi/SpringRestApiApplication.java
@@ -8,5 +8,6 @@ public class SpringRestApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringRestApiApplication.class, args);
+		System.out.println("second");
 	}
 }
